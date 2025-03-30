8760367e338ff49b86fa66c38545a476673de2a1
KonstantinTereshin <tereshin396@gmail.com>
2025-03-30 23:22:45 +0300

    commit 3

---
+++ lesson2/lesson.py
@@ -0,0 +1,13 @@
+# print("start!")
+#
+# x = int(input("Enter number: "))
+#
+# x *= 10
+#
+# # x ** 3
+#
+# print (x)
+#
+x = input("Користувач ввів: ")
+y = x[::-1]
+print("На екрані відображається:", y)
