a37b0527e5d2f6936d1a0af845a77c11bae98262
KonstantinTereshin <tereshin396@gmail.com>
2025-04-11 20:32:37 +0300

    5

---
+++ lesson5/lesson.py
@@ -0,0 +1,35 @@
+# x= "I lik'e Python"
+# y= ('I like Python')
+# z= """I like
+# Python"""
+#
+# print(x)
+# print(y)
+# print(z)
+#
+# y= 100
+# z = str(y)
+# from lesson3.lesson import result
+
+# x= "I lik'e Python"
+#
+# print(x[0])
+# print(len(x))
+# print(x[:6])
+#
+# for elem in x:
+#     print(elem)
+
+# x= "I like Python"
+#
+# print("like" in x)
+#
+# print("I like" + "Python")
+#
+# x= "I like Python"
+# print(x.upper)
+
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+
+print("Hello, %s, %d!" % (name,age))
