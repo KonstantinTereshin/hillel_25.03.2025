153e2a9606f01b1c84095f819f903f2b44996e5e
KonstantinTereshin <tereshin396@gmail.com>
2025-04-11 19:04:01 +0300

    commit

--- lesson3/lesson.py
+++ lesson3/lesson.py
@@ -13,3 +13,34 @@
 # if x:
 #     print("True")
 
+#ДЗ 3.1. Найпростіший калькулятор
+
+
+num1 = float(input("Введіть перше число: "))
+
+
+operation = input("Введіть дію (+, -, *, /): ")
+
+
+num2 = float(input("Введіть друге число: "))
+
+
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1 - num2
+elif operation == '*':
+    result = num1 * num2
+elif operation == '/':
+
+    if num2 == 0:
+        print("Помилка: ділення на нуль!")
+        exit()
+    else:
+        result = num1 / num2
+else:
+    print("Помилка: невідома операція!")
+    exit()
+
+
+print("Результат:", result)
