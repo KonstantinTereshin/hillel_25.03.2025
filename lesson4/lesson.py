cc171813d0cf0f5912a8d721ca34f3237a227704
KonstantinTereshin <tereshin396@gmail.com>
2025-04-11 18:57:04 +0300

    commit

--- lesson4/lesson.py
+++ lesson4/lesson.py
@@ -1,1 +1,19 @@
-1
+# x = range(20, 30, 2)
+# for i in x:
+#     print(i)
+
+# import random
+#
+#
+#
+# print(random.random())
+#
+#
+# print(random.randint(10, 100))
+#
+# lst = [1:4:2]
+#
+
+# tup = (1, 2, 3, 4)
+# print(tup[0])
+
