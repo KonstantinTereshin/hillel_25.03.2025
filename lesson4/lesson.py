bf7858165540a1d947dd2b0ee324f64e7febc73e
KonstantinTereshin <tereshin396@gmail.com>
2025-04-22 01:43:03 +0300

    1

--- lesson4/lesson.py
+++ lesson4/lesson.py
@@ -69,4 +69,32 @@
 # print(calculate_sum_and_multiply([0, 1, 7, 2, 4, 8]))  
 # print(calculate_sum_and_multiply([1, 3, 5]))         
 # print(calculate_sum_and_multiply([6]))                
-# print(calculate_sum_and_multiply([]))                 
+# print(calculate_sum_and_multiply([]))   
+
+# ДЗ 4.3. Список із 3 елементів
+
+import random
+
+
+num_elements = random.randint(3, 10)
+
+
+original_list = [random.randint(3, 10) for _ in range(num_elements)]
+
+print(f"Початковий список: {original_list}")
+
+
+new_list = [original_list[0], original_list[2], original_list[-2]]
+
+print(f"Новий список: {new_list}")
+
+
+print("Приклади:")
+list1 = [1, 2, 3, 4, 5, 6, 7, 9]
+print(f"{list1} == {[list1[0], list1[2], list1[-2]]}")
+
+list2 = [1, 1, 2, 1]
+print(f"{list2} == {[list2[0], list2[2], list2[-2]]}")
+
+list3 = [6, 3, 7]
+print(f"{list3} == {[list3[0], list3[2], list3[-2]]}")   
