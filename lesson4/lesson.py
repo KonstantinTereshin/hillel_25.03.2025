510c80d360fb8b6a04acb95567f9d60f8d7e5d10
KonstantinTereshin <tereshin396@gmail.com>
2025-04-22 01:32:44 +0300

    1

--- lesson4/lesson.py
+++ lesson4/lesson.py
@@ -19,30 +19,54 @@
 
 # ДЗ 4.1. Перемістити всі нулі до кінця списку
 
-def move_zeros_simple(lst):
+# def move_zeros_simple(lst):
 
 
-  non_zeros = [x for x in lst if x != 0]
+#   non_zeros = [x for x in lst if x != 0]
 
 
-  zeros_count = lst.count(0)
+#   zeros_count = lst.count(0)
 
 
-  zeros_list = [0] * zeros_count
+#   zeros_list = [0] * zeros_count
 
 
-  return non_zeros + zeros_list
+#   return non_zeros + zeros_list
 
 
-list1 = [0, 1, 0, 12, 3]
-print(f"{list1} -> {move_zeros_simple(list1)}")
+# list1 = [0, 1, 0, 12, 3]
+# print(f"{list1} -> {move_zeros_simple(list1)}")
 
-list2 = [0]
-print(f"{list2} -> {move_zeros_simple(list2)}")
+# list2 = [0]
+# print(f"{list2} -> {move_zeros_simple(list2)}")
 
-list3 = [1, 0, 13, 0, 0, 0, 5]
-print(f"{list3} -> {move_zeros_simple(list3)}")
+# list3 = [1, 0, 13, 0, 0, 0, 5]
+# print(f"{list3} -> {move_zeros_simple(list3)}")
 
-list4 = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-print(f"{list4} -> {move_zeros_simple(list4)}")
+# list4 = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+# print(f"{list4} -> {move_zeros_simple(list4)}")
 
+# ДЗ 4.2. Знайти суму елементів із парними індексами
+
+# def calculate_sum_and_multiply(data):
+
+
+#   if not data:
+#     return 0
+  
+
+#   sum_even_indexed = sum(data[::2])
+  
+
+#   last_element = data[-1]
+  
+
+#   result = sum_even_indexed * last_element
+  
+#   return result
+
+
+# print(calculate_sum_and_multiply([0, 1, 7, 2, 4, 8]))  
+# print(calculate_sum_and_multiply([1, 3, 5]))         
+# print(calculate_sum_and_multiply([6]))                
+# print(calculate_sum_and_multiply([]))                 
