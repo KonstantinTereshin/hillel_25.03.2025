cffab76a5837fb14a09c6f965fdc7d8463d7e957
KonstantinTereshin <tereshin396@gmail.com>
2025-04-22 01:14:41 +0300

    1

--- lesson3/lesson.py
+++ lesson3/lesson.py
@@ -48,29 +48,53 @@
 
 #ДЗ 3.2. Перемістити елемент у списку
 
-def move_last_to_first(input_list):
+# def move_last_to_first(input_list):
   
 
-  if len(input_list) < 2:
-    return input_list
+#   if len(input_list) < 2:
+#     return input_list
 
-  last_element = input_list[-1]
+#   last_element = input_list[-1]
 
 
-  remaining_elements = input_list[:-1]
+#   remaining_elements = input_list[:-1]
 
-  result_list = [last_element] + remaining_elements
+#   result_list = [last_element] + remaining_elements
 
-  return result_list
+#   return result_list
 
-list1 = [12, 3, 4, 10]
-print(f"{list1} => {move_last_to_first(list1)}")
+# list1 = [12, 3, 4, 10]
+# print(f"{list1} => {move_last_to_first(list1)}")
 
-list2 = [1]
-print(f"{list2} => {move_last_to_first(list2)}")
+# list2 = [1]
+# print(f"{list2} => {move_last_to_first(list2)}")
 
-list3 = []
-print(f"{list3} => {move_last_to_first(list3)}")
+# list3 = []
+# print(f"{list3} => {move_last_to_first(list3)}")
 
-list4 = [12, 3, 4, 10, 8]
-print(f"{list4} => {move_last_to_first(list4)}")
+# list4 = [12, 3, 4, 10, 8]
+# print(f"{list4} => {move_last_to_first(list4)}")
+
+# ДЗ 3.3. Розділити один список на два списки
+
+def split_list(input_list):
+
+  n = len(input_list)
+
+  
+  if n == 0:
+    return [[], []]
+  else:
+    split_point = (n + 1) // 2
+
+    first_half = input_list[:split_point]
+    second_half = input_list[split_point:]
+
+    return [first_half, second_half]
+
+
+print(f"[1, 2, 3, 4, 5, 6] => {split_list([1, 2, 3, 4, 5, 6])}")
+print(f"[1, 2, 3] => {split_list([1, 2, 3])}")
+print(f"[1, 2, 3, 4, 5] => {split_list([1, 2, 3, 4, 5])}")
+print(f"[1] => {split_list([1])}")
+print(f"[] => {split_list([])}")
