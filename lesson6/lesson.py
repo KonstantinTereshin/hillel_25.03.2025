501cda7740e8e4f03089000f186bf4f0f6f387d3
KonstantinTereshin <tereshin396@gmail.com>
2025-04-22 00:57:54 +0300

    1

---
+++ lesson6/lesson.py
@@ -0,0 +1,31 @@
+# x="test"
+
+# x =x.encode("utf-8")
+# print(type(x))
+# print(x)
+
+# x = x.decode("windows-1251")
+
+# print(type(x))
+# print(x)
+
+
+# y = ["Vasya",21,2.1]
+
+# x = {"name": "Vasya",
+#      "age": 21,
+#      "height": 2.1}
+
+# print(y[0])
+# print(x["name"])
+
+# x["city"] = "Dnipro"
+
+# print(x)
+# print(x["city"])
+
+# print("Vasya" in x)
+# print("city" in x)
+
+# print(hash("test"))  
+
